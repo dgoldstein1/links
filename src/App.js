9c79bfcdc78c9b94eb1512bf2673d54ac59ef81b
dgoldstein1 <david.goldstein@deciphernow.com>
2019-09-21 12:31:09 -0400

    prettiered

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 // react + redux
-import React, { Component } from 'react';
-import { Provider } from 'react-redux'
-import { store } from './reducers/index';
+import React, { Component } from "react";
+import { Provider } from "react-redux";
+import { store } from "./reducers/index";
 
 // containers
-import MainView from './containers/mainView';
+import MainView from "./containers/mainView";
 
 // actions
-import { InitAapp } from './actions/appStateActions';
+import { InitAapp } from "./actions/appStateActions";
 
 class App extends Component {
   componentDidMount() {
@@ -18,7 +18,7 @@ class App extends Component {
     return (
       <div className="App">
         <Provider store={store}>
-          <MainView/>
+          <MainView />
         </Provider>
       </div>
     );
