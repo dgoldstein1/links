f4cbf050cfcf00d37d11227aa53781cfc1db1427
dgoldstein1 <david.goldstein@deciphernow.com>
2019-09-21 17:38:21 -0400

    transforming data and doing callback

--- src/actions/graph.js
+++ src/actions/graph.js
@@ -3,6 +3,23 @@ import { store } from "../reducers/index";
 import * as kv from "../api/twowaykv";
 import * as graph from "../api/biggraph";
 
+export const SET_SELECTED_NODE = "SET_SELECTED_NODE";
+export function setSelectedNode(node) {
+  return {
+    type: SET_SELECTED_NODE,
+    node,
+  };
+}
+
+export const ADD_NEIGHBORS_TO_GRAPH = "ADD_NEIGHBORS_TO_GRAPH"
+export function addNeigborsToGraph(node, neighbors) {
+  return {
+    type : ADD_NEIGHBORS_TO_GRAPH,
+    node,
+    neighbors,
+  }
+}
+
 // fetches and stores random starting node and neighbors
 // callback called with string error
 export function fetchAndStoreRandomStartNode(callback) {
@@ -16,7 +33,15 @@ export function fetchAndStoreRandomStartNode(callback) {
       // neighbor ids => values
       kv.entriesFromValues(gr.data).then(nIds => {
         if (nIds.error) return callback(nIds.error);
-        console.log(node, nIds.data.entries);
+        // success! transform data
+        node = {id : node.value, label : node.key}
+        nIds.data.entries = nIds.data.entries || []
+        let neighbors = nIds.data.entries.forEach(n => ({id : n.value, label : n.key}))
+        // set in store
+        store.dispatch(setSelectedNode(node))
+        store.dispatch(addNeigborsToGraph(node, neighbors))
+        return callback()
+
       });
     });
   });
