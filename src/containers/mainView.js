99be9c86eaa3cc857421b2a533cc538592cf42da
dgoldstein1 <david.goldstein@deciphernow.com>
2019-09-22 21:33:55 -0400

    imported mini.css into project

--- src/containers/mainView.js
+++ src/containers/mainView.js
@@ -1,12 +1,18 @@
 /* eslint-disable import/first */
 import React from "react";
 import { connect } from "react-redux";
+import {Header} from "mini.css-react"
 // css
 import "../css/MainView.css";
 
 class MainView extends React.Component {
   render(props) {
-    return <div>GOT HERE</div>;
+    return (
+        <Header sticky>
+          <a logo href='#'>Logo</a>
+          <a href='#'>Home</a>
+        </Header>
+    );
   }
 }
 
