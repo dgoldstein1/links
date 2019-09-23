29763dee462e70892d565d61bacf9729f98da648
dgoldstein1 <david.goldstein@deciphernow.com>
2019-09-23 18:29:20 -0400

    prettiered

--- src/containers/graph.js
+++ src/containers/graph.js
@@ -1,18 +1,24 @@
 /* eslint-disable import/first */
 import React from "react";
 import { connect } from "react-redux";
-import {Sigma, RandomizeNodePositions, RelativeSize} from 'react-sigma';
+import { Sigma, RandomizeNodePositions, RelativeSize } from "react-sigma";
 
-let myGraph = {nodes:[{id:"n1", label:"Alice"}, {id:"n2", label:"Rabbit"}], edges:[{id:"e1",source:"n1",target:"n2",label:"SEES"}]};
+let myGraph = {
+  nodes: [{ id: "n1", label: "Alice" }, { id: "n2", label: "Rabbit" }],
+  edges: [{ id: "e1", source: "n1", target: "n2", label: "SEES" }]
+};
 
 class Graph extends React.Component {
   render() {
     return (
-      <Sigma graph={this.props.graph.graph} settings={{drawEdges: true, clone: false}}>
-  <RelativeSize initialSize={15}/>
-  <RandomizeNodePositions/>
-</Sigma>
-    )
+      <Sigma
+        graph={this.props.graph.graph}
+        settings={{ drawEdges: true, clone: false }}
+      >
+        <RelativeSize initialSize={15} />
+        <RandomizeNodePositions />
+      </Sigma>
+    );
   }
 }
 
