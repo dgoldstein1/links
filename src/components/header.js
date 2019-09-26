60d02417b83dd085fa48a114a044d3e758677db6
dgoldstein1 <david.goldstein@deciphernow.com>
2019-09-26 17:59:23 -0400

    able to toggle mode

--- src/components/header.js
+++ src/components/header.js
@@ -2,20 +2,42 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Header as H } from "mini.css-react";
 import { connect } from "react-redux";
+import { store } from "../reducers";
+import { updateView } from "../actions/appState";
+import { setGraphLoading } from "../actions/graph";
 import "../css/MainView.css";
 
 function Header(p) {
+  let _getSearchBar = () => {
+    if (p.view === "explore") {
+      return <input className="mainBar" type="text" placeholder="search" />;
+    }
+    // p is path
+    return (
+      <>
+        <input className="halfBar" type="text" placeholder="starting from.." />
+        <input className="halfBar" type="text" placeholder="ending at.." />
+      </>
+    );
+  };
+
   return (
     <H sticky className="header">
-      <a>Logo</a>
-      {p.view === "explore" && <input type="text" placeholder="search" />}
-      <button>Mode</button>
-      <button>Layout</button>
-      <button>
+      <a className="floatLeft">Logo</a>
+      {_getSearchBar()}
+      <button
+        onClick={() => {
+          store.dispatch(updateView(p.view === "explore" ? "path" : "explore"));
+        }}
+      >
+        {p.view === "explore" ? "path" : "explore"}
+      </button>
+      <button>{p.layout === "cluster" ? "Hierarchy" : "Cluster"}</button>
+      <button className="floatRight">
         <span className="icon-settings" />
       </button>
-      <button>
-        <span className="icon-info" />
+      <button className="floatRight">
+        <span className="icon-help" />
       </button>
     </H>
   );
