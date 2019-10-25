fbaebd61bf2dfc594b556a116a7874c320389bef
dgoldstein1 <david.goldstein@deciphernow.com>
2019-10-24 22:15:22 -0400

    animation is looking ok

--- src/components/loadingSpinner.js
+++ src/components/loadingSpinner.js
@@ -2,38 +2,54 @@ import React, { useState } from "react";
 import "../css/LoadingSpinner.css";
 import ForceLink from "react-sigma/lib/ForceLink";
 import { Sigma, EdgeShapes, RelativeSize } from "react-sigma";
+import Dagre from "react-sigma/lib/Dagre";
+import PropTypes from "prop-types";
 
 function LoadingSpinner(p) {
+  function _getGraphFromLayout() {
+    if (p.loading === true) {
+      return <Dagre directed={true} easing={true} multigraph={true} />;
+    }
+    // default
+    return (
+      <ForceLink
+        slowDown={1}
+        background
+        easing="cubicInOut"
+        randomize="globally"
+        strongGravityMode={true}
+        gravity={0}
+      />
+    );
+  }
+
   return (
-    <>
-      {p.graph.nodes === [] && <div>TEST</div>}
-      {p.graph.nodes.length > 0 && (
-        <Sigma
-          renderer="canvas"
-          graph={p.graph}
-          settings={{
-            flex: 1,
-            labelThreshold: 0,
-            drawEdges: true,
-            // drawLabels: false,
-            clone: false
-          }}
-          style={{ height: "1000px" }}
-        >
-          <ForceLink
-            slowDown={1}
-            background
-            easing="cubicInOut"
-            randomize="globally"
-            strongGravityMode={true}
-            gravity={0}
-          />
-          <RelativeSize initialSize={35} />
-          <EdgeShapes default="tapered" />
-        </Sigma>
-      )}
-    </>
+    <Sigma
+      renderer="canvas"
+      graph={p.graph}
+      settings={{
+        flex: 1,
+        labelThreshold: 0,
+        drawEdges: true,
+        // drawLabels: false,
+        clone: false,
+        animationsTime: p.animationsTime
+      }}
+      style={{ height: p.height, width: p.width }}
+    >
+      {_getGraphFromLayout()}
+      <RelativeSize initialSize={35} />
+      <EdgeShapes default="tapered" />
+    </Sigma>
   );
 }
+
+LoadingSpinner.propTypes = {
+  animationTime: PropTypes.number.isRequired,
+  graph: PropTypes.object.isRequired,
+  height: PropTypes.string.isRequired,
+  loading: PropTypes.bool.isRequired,
+  width: PropTypes.string.isRequired
+};
 
 export default LoadingSpinner;
--- src/containers/splash.js
+++ src/containers/splash.js
@@ -4,7 +4,7 @@ import { store } from "../reducers";
 import LoadingSpinner from "../components/loadingSpinner";
 // how long to wait on splash
 const SPLASH_TIMEOUT = 10000000;
-const LOADING_ANIMATE_TIME = 5000;
+const LOADING_ANIMATE_TIME = 2000;
 // view after splash page
 const NEXT_VIEW = "path";
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
@@ -19,7 +19,7 @@ function _createRandomGraph() {
   let nodes = [],
     edges = [];
   // create nodes array
-  let nNodes = getRandomInt(3) + 1;
+  let nNodes = getRandomInt(8) + 1;
   for (let i = 0; i < nNodes; i++) {
     nodes.push({
       id: i,
@@ -33,10 +33,26 @@ function _createRandomGraph() {
   nodes.push({
     id: 101
   });
+  nodes.push({
+    id: 102
+  });
+  nodes.push({
+    id: 103
+  });
   edges.push({
     source: 100,
     target: 101,
     id: "100 to 101"
+  });
+  edges.push({
+    source: 100,
+    target: 103,
+    id: "100 to 103"
+  });
+  edges.push({
+    source: 103,
+    target: 102,
+    id: "103 to 102"
   });
   // randomly connect them
   nodes.forEach(n => {
@@ -59,14 +75,15 @@ class Splash extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: true,
       graph: _createRandomGraph()
     };
   }
 
   componentDidMount() {
     setInterval(() => {
-      this.setState({ graph: { nodes: [], edges: [] } });
-      this.setState({ graph: _createRandomGraph() });
+      this.setState({ loading: true });
+      this.setState({ loading: false });
     }, LOADING_ANIMATE_TIME);
     setTimeout(() => {
       store.dispatch(updateView(NEXT_VIEW));
@@ -74,11 +91,16 @@ class Splash extends React.Component {
   }
 
   render() {
-    console.log(this.state.graph);
     return (
       <div>
         SPLASH
-        <LoadingSpinner graph={this.state.graph} />
+        <LoadingSpinner
+          graph={this.state.graph}
+          height={"400px"}
+          loading={this.state.loading}
+          width={"400px"}
+          animationsTime={1200}
+        />
       </div>
     );
   }
