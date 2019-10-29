9f124ce88055ee954cd19c25e0d352c18756a267
dgoldstein1 <david.goldstein@deciphernow.com>
2019-10-29 19:10:46 -0400

    added in outlined areas of text

--- src/components/about.js
+++ src/components/about.js
@@ -36,6 +36,34 @@ function About(p) {
         <a href="#About Me">About Me</a>
         <a href="#Contact">Contact</a>
       </nav>
+      <h1 id="Overview">Overview</h1>
+      <p />
+      <h1 id="Architecture">Architecture</h1>
+      <p />
+      <h5 id="Backend">Backend</h5>
+      <p />
+      <h5 id="Frontend">Frontend</h5>
+      <p />
+      <h5 id="Deployment">Deployment</h5>
+      <p />
+      <h5 id="Metrics">Metrics</h5>
+      <p />
+      <h1 id="Challenges">Challenges</h1>
+      <p />
+      <h5 id="Money">Money</h5>
+      <p />
+      <h5 id="Storage">Storage</h5>
+      <p />
+      <h5 id="Speed">Speed</h5>
+      <p />
+      <h5 id="Wikipedia Refs">Wikipedia Refs</h5>
+      <p />
+      <h1 id="Source Code">Source Code</h1>
+      <p />
+      <h1 id="About Me">About Me</h1>
+      <p />
+      <h1 id="Contact">Contact</h1>
+      <p />
       <Footer />
     </div>
   );
