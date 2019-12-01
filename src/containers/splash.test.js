734de0801c325f59b1627d30e36670ae73eb5bf2
dgoldstein1 <david.goldstein@deciphernow.com>
2019-12-01 13:00:50 -0500

    testing splash page

---
+++ src/containers/splash.test.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Provider } from "react-redux";
+import renderer from "react-test-renderer";
+import configureStore from "redux-mock-store";
+import Splash from "./splash";
+
+const mockStore = configureStore([]);
+
+describe("Splash", () => {
+  const defaultState = {};
+  describe("matches snapshots", () => {
+    let testTable = [
+      {
+        name: "cluster",
+        state: defaultState
+      }
+    ];
+
+    testTable.forEach(t => {
+      it(t.name, () => {
+        let store = mockStore(t.state);
+        let wrapper = renderer.create(
+          <Provider store={store}>
+            <Splash />
+          </Provider>
+        );
+        expect(wrapper).toMatchSnapshot();
+      });
+    });
+  });
+});
