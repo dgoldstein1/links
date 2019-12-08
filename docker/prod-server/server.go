df640e76f4febbe8311c9fd6acb5812ef1ccf533
dgoldstein1 <david.goldstein@deciphernow.com>
2019-12-08 15:09:35 -0500

    created prod-server go file and deleted nginx config

---
+++ docker/prod-server/server.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zsais/go-gin-prometheus"
+	"net/http"
+)
+
+func main() {
+	router := gin.Default()
+	router.Use(gin.Logger())
+	router.LoadHTMLGlob("./build")
+	router.Static("/static", "static")
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+	router.GET("/VERSION", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "VERSION", nil)
+	})
+	router.GET("/LICENSE", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "LICENSE", nil)
+	})
+	// metrics
+	p := ginprometheus.NewPrometheus("gin")
+	p.Use(router)
+	// Start and run the server
+	router.Run(":5000")
+}
