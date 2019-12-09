bf1ef0b6917e8f177f3b42809eff20ef74be0ce3
dgoldstein1 <david.goldstein@deciphernow.com>
2019-12-09 10:15:39 -0500

    able to serve LICENSE and VERSION

--- docker/prod-server/server.go
+++ docker/prod-server/server.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
 	"github.com/zsais/go-gin-prometheus"
+	"log"
 	"net/http"
 	"os"
 )
@@ -15,6 +16,8 @@ func main() {
 
 	router.Use(static.Serve("/", static.LocalFile("./build", true)))
 
+	router.LoadHTMLGlob("./static-files/**")
+	router.Static("/static", "static")
 	router.GET("/VERSION", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "VERSION", nil)
 	})
@@ -25,6 +28,8 @@ func main() {
 	p := ginprometheus.NewPrometheus("gin")
 	p.Use(router)
 	// Start and run the server
-	fmt.Println(os.Getenv("PORT"))
+	if os.Getenv("PORT") == "" {
+		log.Fatalf("cannot serve: no PORT set")
+	}
 	router.Run(fmt.Sprintf(":%s", os.Getenv("PORT")))
 }
