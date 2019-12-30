43bd1f767c9261ad321c13a01a66dd89f5d97ac4
dgoldstein1 <david.goldstein@deciphernow.com>
2019-12-30 14:24:08 -0600

    removed check user ip call

--- src/api/analytics.js
+++ src/api/analytics.js
@@ -12,45 +12,29 @@ import { makeRequest } from "./utils";
  **/
 export function postUserVisit() {
   // first get ip address of user
-  return _getIpAddress().then(ip => {
-    let geoUrl = encodeURI(
-      `/analytics/api/geoIpServer/${ip}?access_key=7eca814a6de384aab338e110c57fef37`
-    );
-    // add in href, if exists
-    return axios
-      .get(geoUrl)
-      .then(res => {
-        if (res.data) {
-          // add in referrer code
-          res.data.href = new URLSearchParams(window.location.search).get(
-            "href"
-          );
-          // small cleanup on attributes
-          res.data.zip_code = res.data.zip;
-        }
-        // now post this data to the backend
-        return makeRequest({
-          method: "post",
-          url: "/analytics/server/visits",
-          onErrorPrefix: "could not post json to analytics backend",
-          body: res.data
-        });
-      })
-      .catch(e => ({
-        success: false,
-        error: "could not get user IP address: " + e.message
-      }));
-  });
-}
-
-export function _getIpAddress() {
   let geoUrl = encodeURI(
-    `/analytics/api/geoIpServer/check?access_key=7eca814a6de384aab338e110c57fef37`
+    `/analytics/api/geoIpServer/v1?apiKey=at_Mb3nWUvk1iAL4W97H5Fs1LxAXjRCn&ipAddress=8.8.8.8`
   );
-  return axios.get(geoUrl).then(r => {
-    // delimit IP address, if needed
-    if (r.data.ip && r.data.ip.includes(","))
-      r.data.ip = r.data.ip.split(",")[0];
-    return Promise.resolve(r.data.ip);
-  });
+  // add in href, if exists
+  return axios
+    .get(geoUrl)
+    .then(res => {
+      if (res.data) {
+        // add in referrer code
+        res.data.href = new URLSearchParams(window.location.search).get("href");
+        // small cleanup on attributes
+        res.data.zip_code = res.data.zip;
+      }
+      // now post this data to the backend
+      return makeRequest({
+        method: "post",
+        url: "/analytics/server/visits",
+        onErrorPrefix: "could not post json to analytics backend",
+        body: res.data
+      });
+    })
+    .catch(e => ({
+      success: false,
+      error: "could not get user IP address: " + e.message
+    }));
 }
